6f31512c67c491a4442e883b480e5da591befd1e
Diego AVZ <124291993+Diego-AVZ@users.noreply.github.com>
2024-03-06 20:47:28 +0100

    Update js1.js

--- js1.js
+++ js1.js
@@ -2,29 +2,10 @@ const web3 = new Web3(window.ethereum);
 
 //CONTRACTS:
 
-const mmContractAddr = "0x5d268A772c6c472ED1582c2b49a84901A2Ec7beA";
+const mmContractAddr = "0xb75d7381FA16b8382F42dFaCa08EcBA90aAB0a49";
 const mmABI = [
 	{
-		"inputs": [
-			{
-				"internalType": "uint8",
-				"name": "amount",
-				"type": "uint8"
-			}
-		],
-		"name": "buyPoints",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint16",
-				"name": "lotId",
-				"type": "uint16"
-			}
-		],
+		"inputs": [],
 		"name": "checkWinner",
 		"outputs": [],
 		"stateMutability": "nonpayable",
@@ -38,27 +19,15 @@ const mmABI = [
 		"type": "function"
 	},
 	{
-		"inputs": [
-			{
-				"internalType": "uint16",
-				"name": "lotId",
-				"type": "uint16"
-			}
-		],
+		"inputs": [],
 		"name": "claimLottery",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
-		"inputs": [
-			{
-				"internalType": "uint16",
-				"name": "lotId",
-				"type": "uint16"
-			}
-		],
-		"name": "closeClaim",
+		"inputs": [],
+		"name": "createAccount",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
@@ -86,6 +55,19 @@ const mmABI = [
 		"name": "depositEth",
 		"outputs": [],
 		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "data",
+				"type": "string"
+			}
+		],
+		"name": "disclaimer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
@@ -175,6 +157,25 @@ const mmABI = [
 		"inputs": [
 			{
 				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "disclaimers",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
 				"name": "user",
 				"type": "address"
 			}
@@ -185,6 +186,19 @@ const mmABI = [
 				"internalType": "uint16",
 				"name": "",
 				"type": "uint16"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getFinalDate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
 			}
 		],
 		"stateMutability": "view",
@@ -215,6 +229,49 @@ const mmABI = [
 				"internalType": "address",
 				"name": "user",
 				"type": "address"
+			},
+			{
+				"internalType": "uint8",
+				"name": "i",
+				"type": "uint8"
+			}
+		],
+		"name": "getMyNums",
+		"outputs": [
+			{
+				"internalType": "uint16",
+				"name": "",
+				"type": "uint16"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "getMyNumsLen",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
 			}
 		],
 		"name": "getMyPuntuation",
@@ -223,6 +280,25 @@ const mmABI = [
 				"internalType": "uint32",
 				"name": "",
 				"type": "uint32"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "getTimesPlayLot",
+		"outputs": [
+			{
+				"internalType": "uint16",
+				"name": "",
+				"type": "uint16"
 			}
 		],
 		"stateMutability": "view",
@@ -242,19 +318,37 @@ const mmABI = [
 		"type": "function"
 	},
 	{
-		"inputs": [
-			{
-				"internalType": "uint16",
-				"name": "lotId",
-				"type": "uint16"
-			}
-		],
+		"inputs": [],
 		"name": "getWinnerNum",
 		"outputs": [
 			{
 				"internalType": "uint256",
 				"name": "",
 				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "hasSign",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
 			}
 		],
 		"stateMutability": "view",
@@ -287,6 +381,75 @@ const mmABI = [
 				"internalType": "uint16",
 				"name": "",
 				"type": "uint16"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "seeIfCanCheckWin",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "seeIfCanDeploy",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "seeIfHasSigned",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "seeIfThereIsALotteryOpen",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
 			}
 		],
 		"stateMutability": "view",
@@ -403,7 +566,7 @@ goBut.addEventListener("click", async()=>{
         const accounts = await web3.eth.getAccounts();
         connectedAddress = accounts[0];
         goBut.innerText = connectedAddress;
-        showEthAddress()
+        showEthAddress();
         goBut.style.background = "black";
         goBut.style.color = "#dffe00";
         goBut.style.borderColor = "#dffe00";
@@ -452,7 +615,7 @@ goBut.addEventListener("click", async()=>{
                 params: [{ chainId: modeTest}], 
               });
             }
-
+            disclaimer()
             walletConnectedEf()
             seeDepositedBalance()
             getDashData()
@@ -518,6 +681,39 @@ async function seeDepositedBalance() {
     } catch(error) {
         console.error(error);
     }
+}
+
+var lotIn = document.getElementById("myNum");
+var ticBut = document.getElementById("ticBut");
+
+async function regNum() {
+	try{
+		var isOpen = await mmContract.methods.seeIfThereIsALotteryOpen().call({from:connectedAddress});
+		if(isOpen == true){
+			await mmContract.methods.regMyNum(lotIn.value).send({from: connectedAddress, value:150000000000000});
+		} else{
+			document.getElementById("msgErrorLot1").style.display = "block";
+		}
+		
+	} catch(Error){console.error(Error);}
+}
+
+async function checkLottery(){
+	try{	
+		var canCheck = await mmContract.methods.seeIfCanCheckWin().call({from: connectedAddress});
+		if(canCheck==true){
+			var lotData = await mmContract.methods.getWinnerNum().call({from: connectedAddress}); // devuelve nº ganador y si eres ganador
+			if(lotData[1] == true){
+				await mmContract.methods.checkWinner().send({from: connectedAddress});
+				/*Add msg for winner */
+			} else {
+				document.getElementById("msgLot1").style.display = "block";
+			}
+		} else {
+			//msg para que esper a que termine la lotería
+			console.log("Wail finish lotery!!!")
+		}
+	}catch(Error){console.error(Error);}
 }
 
 
@@ -592,4 +788,84 @@ async function getDashData() {
         
     }
 }
+
+
+async function disclaimer() {
+    try {
+        
+        var hasSign = await mmContract.methods.seeIfHasSigned(connectedAddress).call();
+
+        if(hasSign[0] == false){
+            document.getElementById("signDisc").style.display = "block";
+        }
+    
+        
+    } catch(error){console.error(error);}
+}
+
+
+async function signDisclaimer() {
+    try {
+
+        var data = `Disclaimer:
+
+        I, as the owner of this public key "${connectedAddress}", hereby accept and acknowledge the following terms and conditions:
+        
+        - Neither the developers nor the founders of the project shall be held liable for any capital losses incurred as a result of interaction with this smart contract.
+        - I acknowledge that I am fully responsible for my actions within this decentralized application (dApp) as the owner of the public key.
+        - Prior to engaging in any transactions within this cryptocurrency project, I understand the importance of conducting thorough due diligence and risk assessment.
+        - I agree to indemnify and hold harmless the developers and founders of the project from any claims, losses, or damages arising from my participation in this venture.
+        - This agreement constitutes a binding contract between myself, as the owner of the public key, and the project, and supersedes any prior agreements or understandings, whether written or oral.
+        
+        By proceeding with transactions, I fully acknowledge and accept the terms and conditions outlined above.`;
+        
+        await mmContract.methods.disclaimer(data).send({from: connectedAddress});
+        
+    } catch(error){console.error(error);}
+}
+
+var dAppBut = document.getElementById("appDash"); 
+var apps = document.getElementById("allDapps"); 
+var aptext =  document.getElementById("aptext");
+var isThere = false;
+
+function showApps(){
+	if(!isThere){
+		apps.style.margin = "4vh -0.3vw";
+		aptext.innerHTML = "↓ dApps ↓";
+		isThere = true;
+		return;
+	} else {
+		apps.style.margin = "100vh -0.3vw";
+		aptext.innerHTML = " ↑ dApps ↑ ";
+		isThere = false;
+	}
+}
+
+var icon1 = document.getElementById("icon1");
+var icon2 = document.getElementById("icon2");
+var icon3 = document.getElementById("icon3");
+var icon4 = document.getElementById("icon4");
+var icon5 = document.getElementById("icon5");
+var icon6 = document.getElementById("icon6");
+
+var bankApp = document.getElementById("bankApp");
+var lotteryApp = document.getElementById("lotteryApp");
+var guide1 = document.getElementById("guide1");
+
+
+
+icon1.addEventListener("click", function(){
+	bankApp.style.display = "block";
+	lotteryApp.style.display = "none";
+	guide1.style.display = "none";
+	showApps();
+})
+
+icon2.addEventListener("click", function(){
+	bankApp.style.display = "none";
+	lotteryApp.style.display = "block";
+	guide1.style.display = "none";
+	showApps();
+})
 
